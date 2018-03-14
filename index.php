72d4046476afdf612c728221162e1003db70e078
Ivo Petkov <ivo@ivopetkov.com>
2018-03-14 10:27:21 +0200

    Code style optimizations.

--- index.php
+++ index.php
@@ -12,9 +12,11 @@ use BearFramework\App;
 $app = App::get();
 $context = $app->context->get(__FILE__);
 
-$context->classes->add('IvoPetkov\BearFrameworkAddons\SMTPSender', 'classes/SMTPSender.php');
+$context->classes
+        ->add('IvoPetkov\BearFrameworkAddons\SMTPSender', 'classes/SMTPSender.php');
 
-$app->emails->registerSender(function() use ($app) {
-    $options = $app->addons->get('ivopetkov/smtp-emails-sender-bearframework-addon')->options;
-    return new IvoPetkov\BearFrameworkAddons\SMTPSender(isset($options['accounts']) && is_array($options['accounts']) ? $options['accounts'] : []);
-});
+$app->emails
+        ->registerSender(function() use ($app) {
+            $options = $app->addons->get('ivopetkov/smtp-emails-sender-bearframework-addon')->options;
+            return new IvoPetkov\BearFrameworkAddons\SMTPSender(isset($options['accounts']) && is_array($options['accounts']) ? $options['accounts'] : []);
+        });
